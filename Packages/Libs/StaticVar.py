1ba97e47a14efae156ba0c26131e2a14d1dd6c49
razevortex <razevortex@googlemail.com>
2023-11-27 09:30:13 +0100

    Create StaticVar.py

---
+++ Packages/Libs/StaticVar.py
@@ -0,0 +1,4 @@
+from Packages.StaticVar import ROOT
+DB_DIR = ROOT + r'/jsons'
+DATA_KEYS_GROUPED = {'Pages': ('Prints', 'Copies', 'ColorPrints', 'ColorCopies'), 'Cartridges': ('B', 'C', 'M', 'Y'), 'Time': ('Date',)}
+DATA_KEYS_LIST = ('Prints', 'Copies', 'ColorPrints', 'ColorCopies', 'B', 'C', 'M', 'Y', 'Date')
