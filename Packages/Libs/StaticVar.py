63e00a980cbe4e38ba4e9e478232d3b9976f0974
razevortex <razevortex@googlemail.com>
2023-11-27 09:57:56 +0100

    Update StaticVar.py

--- Packages/Libs/StaticVar.py
+++ Packages/Libs/StaticVar.py
@@ -1,4 +1,3 @@
 from Packages.StaticVar import ROOT
-DB_DIR = ROOT + r'/jsons'
 DATA_KEYS_GROUPED = {'Pages': ('Prints', 'Copies', 'ColorPrints', 'ColorCopies'), 'Cartridges': ('B', 'C', 'M', 'Y'), 'Time': ('Date',)}
 DATA_KEYS_LIST = ('Prints', 'Copies', 'ColorPrints', 'ColorCopies', 'B', 'C', 'M', 'Y', 'Date')
