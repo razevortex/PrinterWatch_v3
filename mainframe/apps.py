bb3d3dff06f438cc1df388d219aef942da15e288
razevortex <razevortex@googlemail.com>
2024-02-01 16:53:50 +0100

    Delete mainframe directory

--- mainframe/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainframeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainframe'
--- mainframe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.mainframeset),
-    path('signup/', views.signup),
-    path('help/', views.help),
-]
