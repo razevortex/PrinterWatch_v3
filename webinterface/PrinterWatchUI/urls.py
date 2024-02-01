521d1d6ee43051231d9fefd21fc7bad53edd3049
razevortex <razevortex@googlemail.com>
2024-02-01 16:53:27 +0100

    Delete webinterface directory

--- webinterface/PrinterWatchUI/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("page/", views.mainpage, name="mainpage"),
-    path("plot/", views.plot, name="plot"),
-    path("login/", views.login, name="login"),
-]
